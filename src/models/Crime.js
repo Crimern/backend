f8324371ef322376d4f2fb1d021576d2fc97e721
Rekseto <ReksetoAjx@gmail.com>
2018-08-06 12:03:10 +0200

    Introducing GeoCoordinates into Crime

--- src/models/Crime.js
+++ src/models/Crime.js
@@ -1,10 +1,13 @@
 import autoIncrement from 'mongoose-auto-increment';
+import mongooseDecimal from 'mongoose-big-decimal';
 export default (mongoose) => {
+  mongooseDecimal(mongoose)
   const Schema = mongoose.Schema;
+  
 
   const crime = new Schema({
-    x: { type: Number, required: true},
-    y: { type: Number, required: true },
+    x: { type:  mongoose.Schema.Types.BigDecimal, required: true},
+    y: { type:  mongoose.Schema.Types.BigDecimal, required: true },
     date: { type: Date, required: true },
     type : {type: mongoose.Schema.Types.ObjectId, ref: 'CrimeType' },
     created_at: Date,
