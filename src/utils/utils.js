588ac67dba0691801883ae9e70ca577ee93db41f
Rekseto <ReksetoAjx@gmail.com>
2018-08-09 16:00:53 +0200

    Zoom fix

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,8 @@
 export const radiusRelatedZoom = zoom => {
   if(zoom > 10) {
-    return 18-zoom+1.5;
+    return 18-zoom*5;
   } 
   if(zoom < 10) {
-    return 18-zoom+3.5;
+    return (18-zoom)*15;
   }
 }
