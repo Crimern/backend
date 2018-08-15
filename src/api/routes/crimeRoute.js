ac1604493bb36a87f5eb1e792e2cc84914bad65b
Rekseto <ReksetoAjx@gmail.com>
2018-08-16 00:54:01 +0200

    Documentation make up

--- src/api/routes/crimeRoute.js
+++ src/api/routes/crimeRoute.js
@@ -1,5 +1,5 @@
 import config from "config";
-import controller, {get,post} from "inra-server-http/dest/router";
+import controller, { get, post } from "inra-server-http/dest/router";
 import CrimeService from "../services/crimeService";
 
 
@@ -11,36 +11,61 @@ export default class CrimeRouter {
     this.crimeService = CrimeService(this.database)
   }
 
-
+  /**
+  * @api {post} /api/crimeType Add new crimeType
+  * @apiName CreateCrimeType
+  * @apiGroup CrimeType
+  *
+  * @apiParam {String} name 
+  * 
+  * @apiSuccess {Object} created object.
+  */
   @post('/crime')
   async create(ctx) {
-      const data = await this.crimeService.create(ctx.request.body)
-      
-      ctx.body = {
-        success: true,
-        data: data
-      }
+    const data = await this.crimeService.create(ctx.request.body)
+
+    ctx.body = {
+      success: true,
+      data: data
+    }
 
   }
-
+  /**
+  * @api {post} /api/crime Fetches all crimes
+  * @apiName FetchCrimes
+  * @apiGroup Crime
+  *
+  * 
+  * @apiSuccess {Object} Array with all crimes.
+  */
   @get('/crime')
   async fetch(ctx) {
-      const data = await this.crimeService.fetchAll()
-      
-      ctx.body = {
-        success: true,
-        data: data
-      }
+    const data = await this.crimeService.fetchAll()
+
+    ctx.body = {
+      success: true,
+      data: data
+    }
   }
 
+  /**
+* @api {post} /api/crime Fetches all crimes in radius
+* @apiName FetchCrimesRadius
+* @apiGroup Crime
+*
+* @apiParam {Array} types 
+* @apiParam {Number} lng longtidude 
+* @apiParam {Number} lat latidude
+* @apiSuccess {Object} Array with all crimes in given radius.
+*/
   @post('/crime/radius')
   async fetchInRadius(ctx) {
-      const data = await this.crimeService.fetchInRadius(JSON.parse(ctx.request.body))
-      
-      ctx.body = {
-        success: true,
-        data: data
-      }
+    const data = await this.crimeService.fetchInRadi  us(JSON.parse(ctx.request.body))
+
+    ctx.body = {
+      success: true,
+      data: data
+    }
   }
 
 }
--- src/api/routes/crimeTypeRoute.js
+++ src/api/routes/crimeTypeRoute.js
@@ -1,6 +1,6 @@
 import config from "config";
 import CrimeTypeService from "../services/crimeTypeService";
-import controller, {get,post} from "inra-server-http/dest/router";
+import controller, { get, post } from "inra-server-http/dest/router";
 
 @controller('/api')
 export default class CrimeTypeRouter {
@@ -10,30 +10,41 @@ export default class CrimeTypeRouter {
     this.crimeTypeService = CrimeTypeService(this.database)
   }
 
+  /**
+ * @api {post} /api/crimeType Add new crimeType
+ * @apiName CreateCrimeType
+ * @apiGroup CrimeType
+ *
+ * @apiParam {String} name 
+ * 
+ * @apiSuccess {Object} created object.
+ */
   @post('/crimeType')
   async create(ctx) {
-    try {
-      const data = await this.crimeTypeService.create(ctx.request.body)
-      
-      ctx.body = {
-        success: true,
-        data: data
-      }
-    } catch (error) {
-      ctx.body = {
-        success: false,
-        error: error.message
-      }
+    const data = await this.crimeTypeService.create(ctx.request.body)
+
+    ctx.status = 201;
+    ctx.body = {
+      success: true,
+      data: data
     }
   }
 
+  /**
+ * @api {post} /api/crimeType Fetches all CrimeTypes
+ * @apiName GetCrimeType
+ * @apiGroup CrimeType
+ *
+ * 
+ * @apiSuccess {Object} requested objects.
+ */
   @get('/crimeType')
   async fetchAll(ctx) {
-      const data = await this.crimeTypeService.fetchAll()
-      
-      ctx.body = {
-        success: true,
-        data: data
-      }
+    const data = await this.crimeTypeService.fetchAll()
+
+    ctx.body = {
+      success: true,
+      data: data
+    }
   }
 }
